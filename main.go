8ef0878ae70c2966dc68dc409fe5e7aa364f2593
lhristijan <hristijan.lubeski@keitaro.com>
2018-08-29 10:14:55 +0200

    Improves logging for the main.go file

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"log"
 
 	"github.com/Microkubes/microservice-apps-management/app"
 	pkgdb "github.com/Microkubes/microservice-apps-management/db"
@@ -22,57 +23,75 @@ import (
 func main() {
 	// Create service
 	service := goa.New("apps-management")
+	service.LogInfo("Apps-management service created.")
 
 	// Load configuration
+	service.LogInfo("Gateway settings config loading ...")
 	gatewayAdminURL, configFile := loadGatewaySettings()
 
 	cfg, err := toolscfg.LoadConfig(configFile)
 	if err != nil {
-		service.LogError("config", "err", err)
+		service.LogError("Error with loading the config!", "Error:", err)
 		return
 	}
+	service.LogInfo("Successfully loaded config file.")
+
 
 	// Gateway self-registration
+	service.LogInfo("Registering microservice...")
 	unregisterService := registerMicroservice(gatewayAdminURL, cfg)
+	service.LogInfo("Microservice registered.")
 	defer unregisterService() // defer the unregister for after main exits
 
 	// Setup apps-management service
+	service.LogInfo("Setting up apps-management service...")
 	appsService, err := setupAppsService(cfg)
 	if err != nil {
-		service.LogError("config", err)
+		service.LogError("Error with setting up apps-management service!", "Error:", err)
+		return
 	}
+	service.LogInfo("Setting up apps-management service went successfully.")
 
 	// create security chain
+	service.LogInfo("Creating security chain ...")
 	securityChain, cleanup, err := flow.NewSecurityFromConfig(cfg)
 	if err != nil {
-		panic(err)
+		service.LogError("Error creating security chain!", "Error:", err)
+		return
 	}
 	defer cleanup()
 
 	// Mount middleware
+	service.LogInfo("Mounting middlewares and chains...")
 	service.Use(middleware.RequestID())
 	service.Use(middleware.LogRequest(true))
 	service.Use(middleware.ErrorHandler(service, true))
 	service.Use(middleware.Recover())
 
 	service.Use(chain.AsGoaMiddleware(securityChain))
+	service.LogInfo("Successfully mounted middlewares and chains!")
+
 
 	// Mount "apps" controller
+	service.LogInfo("Creating a new namespace controller...")
 	c := NewAppsController(service, appsService)
 	app.MountAppsController(service, c)
 
 	// Mount "swagger" controller
-	// c2 := NewSwaggerController(service)
-	// app.MountSwaggerController(service, c2)
+	service.LogInfo("Mounting the swagger controller...")
+	c2 := NewSwaggerController(service)
+	app.MountSwaggerController(service, c2)
 
 	// Start service
+	service.LogInfo("Starting the service ...")
 	if err := service.ListenAndServe(fmt.Sprintf(":%d", cfg.Service.MicroservicePort)); err != nil {
-		service.LogError("startup", "err", err)
+		service.LogError("Starting the service failed!", "Error:", err)
 	}
 
 }
 
 func setupRepository(backend backends.Backend) (backends.Repository, error) {
+	defer log.Print("Setting up repository went successfully!")
 	return backend.DefineRepository("apps-management", backends.RepositoryDefinitionMap{
 		"name": "apps-management",
 		"indexes": []backends.Index{
@@ -93,10 +112,12 @@ func setupRepository(backend backends.Backend) (backends.Repository, error) {
 }
 
 func setupBackend(dbConfig toolscfg.DBConfig) (backends.Backend, backends.BackendManager, error) {
+	log.Print("Setting up backends...")
 	dbinfoMap := map[string]*toolscfg.DBInfo{}
 	dbinfoMap[dbConfig.DBName] = &dbConfig.DBInfo
 	backendManager := backends.NewBackendSupport(dbinfoMap)
 	backend, err := backendManager.GetBackend(dbConfig.DBName)
+	log.Print("Setting up backends went successfully.")
 	return backend, backendManager, err
 }
 
